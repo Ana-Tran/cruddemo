8861c317986fbacdc46555b3defa629571f50eb6
Ana-Tran <anatran334@gmail.com>
2023-11-15 17:43:11 -0500

    create class to implement interface, create entity manager for dependency injection, write save method code

---
+++ src/main/java/com/tranana/cruddemo/dao/StudentDAOImpl.java
@@ -0,0 +1,27 @@
+package com.tranana.cruddemo.dao;
+
+import com.tranana.cruddemo.entity.Student;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository //gives support for component scanning and handles spring support for exceptions translations
+public class StudentDAOImpl implements StudentDAO{
+
+    // define field for entity manager
+    private EntityManager entityManager;
+
+    // inject entity manager using constructor injection
+    @Autowired
+    public StudentDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    // implement save,method
+    @Override
+    @Transactional   // used since we are performing an update on the database from spring framework
+    public void save(Student theStudent) {
+        entityManager.persist(theStudent); //saves student to the database
+    }
+}
